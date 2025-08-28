bdfbde5c2ce227c2b057df5c928d9f209411fd92
Eugene Evstratev <levipupsik321@gmail.com>
2025-08-28 20:58:20 +0300

    Сделал name приватным, сделал геттер, сеттер для name, проверку на длину, classmethod и staticmethod работаю корректно.

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +16,9 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-
-        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +33,43 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= Item.pay_rate
+
+    @property
+    def name(self) -> str:
+        """
+        name getter
+        :return: Возвращает имя
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, new_name: str) -> None:
+        """
+        name setter, проверяет на длину слова. Если больше 10 - обрезает до первых 10 символов.
+        :param new_name: Новое имя
+        """
+        if len(new_name) < 10:
+            self.__name = new_name
+        else:
+            self.__name = new_name[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, file_path: str) -> None:
+        """
+        Класс метод, открывает файл items.csv с помощью контекстного менеджера, и создает экземпляры класса в атрибут класса all.
+        :param file_path: Путь до файла.
+        """
+        with open(file=file_path, newline='', encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for element in reader:
+                Item.all.append(
+                    cls(name=element['name'], price=float(element['price']), quantity=int(element['quantity'])))
+
+    @staticmethod
+    def string_to_number(user_string: str) -> int:
+        """
+        Статичный метод, делает из строки число, округляет до целого числа "в пол".
+        :param user_string: Строка пользователя.
+        :return: Число.
+        """
+        return int(float(user_string))
